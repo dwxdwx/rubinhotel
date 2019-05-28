0f44438f9592b330379d24c44ec8523f3984cb0c
dwxdwx <530320086@qq.com>
2019-05-28 15:44:50 +0800

    修改了连接数据库失败的问题

--- src/rubin/util/JsonUtil.java
+++ src/rubin/util/JsonUtil.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonUtil {
-
 	//实现将参数obj对象序列化为json格式的字符串
 	public static String obj2String(Object obj) {
 		try {
